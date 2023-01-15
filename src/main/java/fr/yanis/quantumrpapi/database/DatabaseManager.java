05103bf6a00c0bf29ce77096a49ca3598439c9c1
Sterll <77593289+Sterll@users.noreply.github.com>
2023-01-15 01:43:09 +0100

    Update DatabaseManager.java

--- src/main/java/fr/yanis/quantumrpapi/database/DatabaseManager.java
+++ src/main/java/fr/yanis/quantumrpapi/database/DatabaseManager.java
@@ -2,7 +2,7 @@ package fr.yanis.quantumrpapi.database;
 
 public enum DatabaseManager {
 
-    QuantumRPBDD(new DatabaseCredentials("65.21.131.103", "u17_7f9q2SLwWP", "7=i89LRsdr2T0eYZid=tjP+R", "s17_QuantumRP", 3306));
+    QuantumRPBDD(new DatabaseCredentials("UNABLETOREAD", "UNABLETOREAD", "UNABLETOREAD", "UNABLETOREAD", 3306));
 
     private DatabaseAccess databaseAccess;
     DatabaseManager(DatabaseCredentials credentials){
